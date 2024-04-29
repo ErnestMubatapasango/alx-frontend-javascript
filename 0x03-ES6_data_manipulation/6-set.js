817cef1a857a39e7acc61b832e437556aac196ee
ErnestMubatapasango <tawazzmubazz@gmail.com>
2024-04-29 12:27:26 +0200

    array buffer and set methods

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+function setFromArray(array) {
+    // Create a new Set from the array
+    return new Set(array);
+}
+
+// Example usage:
+const array = [1, 2, 3, 4, 5];
+const set = setFromArray(array);
+console.log(set);
